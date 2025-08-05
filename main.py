1b38f5c091b2c0e3b3a1ffbc74afd91050f82a5a
DuelistRag3 <christian.dullin@web.de>
2025-08-05 15:02:46 +0200

    update

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands, tasks
-import Database.connection as db
 
 # Load environment variables from .env file
 load_dotenv()
@@ -13,12 +12,6 @@ class Client(commands.Bot):
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
-
-        try:
-            db.get_connection()
-            print("Database connection established successfully.")
-        except Exception as e:
-            print(f"Error establishing database connection: {e}")
 
 intents = discord.Intents.default()
 intents.messages = True
