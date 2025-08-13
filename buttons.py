664d78306f7abe9cde5df55fb6243cd24a8fdead
DuelistRag3 <christian.dullin@web.de>
2025-08-13 07:40:10 +0200

    using new env variable in button

--- buttons.py
+++ buttons.py
@@ -1,7 +1,11 @@
+import os
 import discord
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class ConnectButton(discord.ui.View):
     def __init__(self, addr, port):
         super().__init__(timeout=30)
-        button = discord.ui.Button(label="Connect", style=discord.ButtonStyle.green, url=f"https://connect.die-taverne.eu/{addr}/{port}")
+        button = discord.ui.Button(label="Connect", style=discord.ButtonStyle.green, url=f"{os.getenv('STEAM_CONNECT_URL')}/{addr}/{port}")
         self.add_item(button)
